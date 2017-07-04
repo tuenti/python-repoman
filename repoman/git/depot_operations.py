0dc615cd61f02048ab226f120263363cf778918d
Óscar San José <oscar@tuenti.com>
2017-07-04 19:15:56 +0200

    Retry if there is en exception in shutil.rmtree

--- repoman/git/depot_operations.py
+++ repoman/git/depot_operations.py
@@ -105,7 +105,11 @@ class DepotOperations(BaseDepotOps):
         refs_dir = os.path.join(git_path, self._REFS_DIR)
         refs_previous_dir = os.path.join(git_path, self._REFS_PREVIOUS_DIR)
         if os.path.isdir(refs_previous_dir):
-            shutil.rmtree(refs_previous_dir)
+            try:
+                shutil.rmtree(refs_previous_dir)
+            except OSError:
+                #retry, bug in shutil: http://code.activestate.com/lists/python-list/159050/
+                shutil.rmtree(refs_previous_dir)
         shutil.copytree(refs_dir, refs_previous_dir)
 
     def _restore_state_refs(self, git_path):
